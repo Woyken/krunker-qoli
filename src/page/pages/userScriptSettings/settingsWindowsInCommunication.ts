d5a02adc7abbdfe90be54b2117e17413dae782f3
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-21 19:29:04 +0300

    fiddling with current state message

--- src/page/pages/userScriptSettings/settingsWindowsInCommunication.ts
+++ src/page/pages/userScriptSettings/settingsWindowsInCommunication.ts
@@ -1,53 +1,85 @@
 import { expose, windowEndpoint } from "comlink";
-import { createEffect } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import {
   enabledFastRespawn,
   enabledAdPopupRemoval,
 } from "./state/userScriptSettings";
-
-// TODO wrap all this login into function that is called in settings page
-const onUnloadPromise = new Promise<void>((resolve) => {
-  window.addEventListener("beforeunload", () => {
-    resolve();
-  });
-});
 
 export interface UserScriptSettings {
   enabledFastRespawn: boolean;
   enabledAdPopupRemoval: boolean;
 }
 
-// TODO const [currentCallback, setCurrentCallback] = createSignal<(settings: UserScriptSettings) => void>();
-let currentCallback: ((settings: UserScriptSettings) => void) | undefined;
-
-const exposedSettings = {
-  onUnloadPromise,
-  doStufff: () => {
-    console.log("TODO doStufff");
-    return Math.random();
-  },
+export interface ExposedSettings {
+  onUnloadPromise: Promise<void>;
+  doStufff: () => number;
   registerCallback: (
     apiVersion: string,
     callback: (settings: UserScriptSettings) => void
-  ) => {
-    currentCallback = callback;
-    // callback just registered, send them an update as soon as possible
-    currentCallback({
+  ) => void;
+  ping: number;
+}
+
+export enum SettingsCommunicationState {
+  NoOpener,
+  WaitingForCallbackRegistration,
+  ReadyToPushEvents,
+}
+
+export function useSettingsCommunication() {
+  const [communicatorState, setCommunicatorState] = createSignal(
+    SettingsCommunicationState.NoOpener
+  );
+
+  if (!window.opener) return { communicatorState };
+  setCommunicatorState(
+    SettingsCommunicationState.WaitingForCallbackRegistration
+  );
+
+  const windowOnUnloadPromise = new Promise<void>((resolve) => {
+    window.addEventListener("beforeunload", () => {
+      resolve();
+    });
+  });
+
+  const [currentCallback, setCurrentCallback] =
+    createSignal<(settings: UserScriptSettings) => void>();
+
+  const exposedSettings: ExposedSettings = {
+    onUnloadPromise: windowOnUnloadPromise,
+    doStufff: () => {
+      console.log("TODO doStufff");
+      return Math.random();
+    },
+    registerCallback: (
+      apiVersion: string,
+      callback: (settings: UserScriptSettings) => void
+    ) => {
+      setCurrentCallback(() => callback);
+
+      if (
+        communicatorState() ===
+        SettingsCommunicationState.WaitingForCallbackRegistration
+      )
+        setCommunicatorState(SettingsCommunicationState.ReadyToPushEvents);
+
+      // callback just registered, send them an update as soon as possible
+      callback({
+        enabledFastRespawn: enabledFastRespawn(),
+        enabledAdPopupRemoval: enabledAdPopupRemoval(),
+      });
+    },
+    ping: 0,
+  };
+
+  createEffect(() => {
+    currentCallback()?.({
       enabledFastRespawn: enabledFastRespawn(),
       enabledAdPopupRemoval: enabledAdPopupRemoval(),
     });
-  },
-  ping: 0,
-};
-export type exposedSettings = typeof exposedSettings;
-
-createEffect(() => {
-  const enabledFastRespawnB = enabledFastRespawn();
-  const enabledAdPopupRemovalB = enabledAdPopupRemoval();
-  currentCallback?.({
-    enabledFastRespawn: enabledFastRespawnB,
-    enabledAdPopupRemoval: enabledAdPopupRemovalB,
   });
-});
 
-expose(exposedSettings, windowEndpoint(window.opener));
+  expose(exposedSettings, windowEndpoint(window.opener));
+
+  return { communicatorState };
+}
