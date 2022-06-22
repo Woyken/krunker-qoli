5db15538af0673265ef3111d5be71219c1c1b1e1
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-23 00:04:35 +0300

    reuse already existing localDocument

--- src/userScript/state/documentState.ts
+++ src/userScript/state/documentState.ts
@@ -1,18 +1,16 @@
 import { createSignal } from 'solid-js';
+import localDocument from '../utils/localDocumentCopy';
 import createScopedLogger from '../utils/logger';
 
 const logger = createScopedLogger('[documentState]');
-
-const localDocumentAddEventListener = document.addEventListener.bind(document);
-const localDocumentRemoveEventListener = document.removeEventListener.bind(document);
 
 const [documentReadyStateIsComplete, setDocumentReadyStateIsComplete] = createSignal(false);
 
 if (document.readyState !== 'complete') {
     // log("document readyState", document.readyState, "adding listener, waiting for reasyState complete event");
-    localDocumentAddEventListener('readystatechange', function readyStateChange() {
+    localDocument.addEventListener('readystatechange', function readyStateChange() {
         if (document.readyState === 'complete') {
-            localDocumentRemoveEventListener('readystatechange', readyStateChange);
+            localDocument.removeEventListener('readystatechange', readyStateChange);
             logger.log('document readyState complete event received, setting state true');
             setDocumentReadyStateIsComplete(true);
         }
