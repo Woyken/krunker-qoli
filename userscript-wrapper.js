0eaa21eaccd2de4b0ae268870f8ec0ecb7fb122c
Woyken <Woyken@users.noreply.github.com>
2022-06-27 14:14:07 +0000

    deploy: d8d9fa1ed6e9ded8643daa304ea6a92af47e301b

--- userscript-wrapper.js
+++ userscript-wrapper.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Krunker Quoli
 // @namespace    https://github.com/Woyken/krunker-qoli
-// @version      0.5.1
+// @version      0.5.2
 // @description  
 // @author       Woyken
 // @match        https://krunker.io/*
