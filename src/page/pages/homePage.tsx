fd5e8ef79a37cc89b5de4935012bd1516013a5b7
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-27 17:35:24 +0300

    Added home page link to window manager

--- src/page/pages/homePage.tsx
+++ src/page/pages/homePage.tsx
@@ -1,14 +1,28 @@
 import Button from '@suid/material/Button';
 import Typography from '@suid/material/Typography';
+import { useNavigate } from 'solid-app-router';
 
 export default function HomePage() {
-    const handleInstallUserScript = () => {
-        if (window.location.host.startsWith('localhost')) window.location.href = new URL('src/userScript/script.user.ts', window.location.href).href;
-        else window.location.href = new URL('script.user.js', window.location.href).href;
-    };
-    const handleOpenKrunker = () => {
+    const navigate = useNavigate();
+
+    function handleOpenWindowManager() {
+        navigate('/windowManager');
+    }
+
+    function handleInstallUserScript() {
+        let url: string;
+        if (window.location.host.startsWith('localhost')) url = new URL('src/userScript/script.user.ts', window.location.href).href;
+        else url = new URL('script.user.js', window.location.href).href;
+        window.open(url);
+        setTimeout(() => {
+            handleOpenWindowManager();
+        }, 200);
+    }
+
+    function handleOpenKrunker() {
         window.open('https://krunker.io', '_self');
-    };
+    }
+
     return (
         <>
             <Typography variant="h2" component="div" gutterBottom>
@@ -16,7 +30,9 @@ export default function HomePage() {
             </Typography>
             <Button onclick={handleInstallUserScript}>Install userscript</Button>
             <br />
-            <Button onclick={handleOpenKrunker}>Open krunker</Button>
+            <Button onclick={handleOpenWindowManager}>Open Krunker window manager</Button>
+            <br />
+            <Button onclick={handleOpenKrunker}>Open Krunker</Button>
         </>
     );
 }
