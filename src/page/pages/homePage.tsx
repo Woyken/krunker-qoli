c04003d4c0df872827a5fd900545e861a547ef6d
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-27 17:25:14 +0300

    Added button to open krunker

--- src/page/pages/homePage.tsx
+++ src/page/pages/homePage.tsx
@@ -6,12 +6,17 @@ export default function HomePage() {
         if (window.location.host.startsWith('localhost')) window.location.href = new URL('src/userScript/script.user.ts', window.location.href).href;
         else window.location.href = new URL('script.user.js', window.location.href).href;
     };
+    const handleOpenKrunker = () => {
+        window.open('https://krunker.io', '_self');
+    };
     return (
         <>
             <Typography variant="h2" component="div" gutterBottom>
                 Krunker Qoli
             </Typography>
-            <Button onclick={handleInstallUserScript}> install userscript</Button>
+            <Button onclick={handleInstallUserScript}>Install userscript</Button>
+            <br />
+            <Button onclick={handleOpenKrunker}>Open krunker</Button>
         </>
     );
 }
