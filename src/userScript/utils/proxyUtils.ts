1f7ac4136463fea4f3d1f8cad854ff46b1b24a95
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-28 09:48:55 +0300

    👌 IMPROVE: function proxy

--- src/userScript/utils/proxyUtils.ts
+++ src/userScript/utils/proxyUtils.ts
@@ -1,14 +1,54 @@
+import createScopedLogger from './logger';
+
+const localFunctionApplyApply = Function.prototype.apply.apply.bind(Function.prototype.apply as any);
+
+function isErrorWithStack(param: unknown): param is { stack: string } {
+    if (typeof param !== 'object') return false;
+    if (param == null) return false;
+    return 'stack' in param;
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export function createFunctionProxy<T extends (...arg: never[]) => unknown>(
     originalTarget: T,
     beforeCallback?: (...param: Parameters<T>) => void,
     afterCallback?: (...param: Parameters<T>) => void
 ) {
+    const logger = createScopedLogger('[createFunctionProxy]', originalTarget, beforeCallback, afterCallback);
+    logger.log('creating function proxy');
     return new Proxy(originalTarget, {
         apply(target, thisArg, argArray: Parameters<T>) {
-            beforeCallback?.(...argArray);
-            const result = target.apply(thisArg, argArray);
-            afterCallback?.(...argArray);
+            const funcLogger = logger.createScopedLogger('[apply]', target, thisArg, argArray);
+            try {
+                beforeCallback?.(...argArray);
+            } catch (error) {
+                funcLogger.log('beforeCallback error', error);
+            }
+            let result: unknown;
+            try {
+                result = localFunctionApplyApply(target, [thisArg, argArray]);
+            } catch (e) {
+                funcLogger.log('EXCEPTION', e);
+                // Remove this function from call stack
+                if (isErrorWithStack(e)) {
+                    if (e.stack.includes('Object.apply')) {
+                        // Probably chrome
+                        // Remove first Object.apply, this is this function
+                        e.stack = e.stack.replace(/\n.*Object\.apply.*/, '');
+                    } else if (e.stack.includes('apply@')) {
+                        // Probably firefox
+                        // Remove first apply@
+                        e.stack = e.stack.replace(/.*apply@.*\n/, '');
+                    }
+                }
+                funcLogger.log('EXCEPTION before rethrow', e);
+                throw e;
+            }
+            try {
+                afterCallback?.(...argArray);
+            } catch (error) {
+                funcLogger.log('afterCallback error', error);
+            }
             return result;
         },
     });
