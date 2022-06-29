b36c901752f755323efc410501aff647c7f36aea
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-29 11:58:44 +0300

    Fiddling with dynamic class creation to extend Array

--- src/shared/globalOverrides/Array.ts
+++ src/shared/globalOverrides/Array.ts
@@ -1,8 +1,23 @@
 import { getClonedPrototype, defineAndBindFunctionsFrom } from './fixedPrototype';
 
-class CustomArray extends Array {}
+// class CustomArray2 extends Array {}
 
+const ClassFactory = (superclass: any, customPrototype: any) =>
+    class extends superclass {
+        constructor(...items: unknown[]) {
+            super(...items);
+            Object.setPrototypeOf(this, customPrototype);
+        }
+    };
+
+const CustomArray = ClassFactory(Array, getClonedPrototype(Array.prototype));
 defineAndBindFunctionsFrom(CustomArray, Array);
-TODO = getClonedPrototype(Array.prototype, CustomArray.prototype);
+// const extendsAnyClass = (AnyClass) => class MyMixinClass extends MixinClass(AnyClass) {};
+
+// make [[prototype]] of Child be a ref to Parent.prototype
+// CustomArray.prototype = Object.create(Array.prototype);
+
+// need to explicitly set the constructor
+// CustomArray.prototype.constructor = CustomArray;
 
 export default CustomArray;
