cc675b90a1f67bd098f5e80614a79e008534da32
Woyken <13166802+Woyken@users.noreply.github.com>
2022-06-29 23:23:16 +0300

    Added custom build step, replacing all ArrayExpressions with Array constructor calls. This allows to more arrays with global overrides

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { resolve } from 'path';
 import Checker from 'vite-plugin-checker';
 import eslint from 'vite-plugin-eslint';
 import inject from '@rollup/plugin-inject';
+import replaceArrayExpressionToConstructor from './buildTools/replaceArrayExpressionToConstructor';
 import pkg from './package.json';
 
 const loaderModuleBanner = `
@@ -39,31 +40,36 @@ else vitePlugins.push(solidPlugin());
 const rollupPlugins: BuildOptions['rollupOptions']['plugins'] = [];
 if (isScriptBuild)
     vitePlugins.push(
-        // Overriding globals with custom objects. Copying and saving original prototype functions
-        // This way we can avoid prototype overrides. Will always call original function
-        // Some functions/properties might be undefined, need to check manually
-        inject({
-            // files called "unsafe.***" will be ignored
-            include: /(?!.*\/(unsafe\.|globalOverrides))(src\/.*)|(node_modules)/,
-            Array: '@/shared/globalOverrides/Array',
-            console: '@/shared/globalOverrides/console',
-            window: '@/shared/globalOverrides/window',
-            document: '@/shared/globalOverrides/document',
-            Error: '@/shared/globalOverrides/Error',
-            KeyboardEvent: '@/shared/globalOverrides/KeyboardEvent',
-            Map: '@/shared/globalOverrides/Map',
-            Math: '@/shared/globalOverrides/Math',
-            MessageChannel: '@/shared/globalOverrides/MessageChannel',
-            MouseEvent: '@/shared/globalOverrides/MouseEvent',
-            MutationObserver: '@/shared/globalOverrides/MutationObserver',
-            Number: '@/shared/globalOverrides/Number',
-            Object: '@/shared/globalOverrides/Object',
-            Promise: '@/shared/globalOverrides/Promise',
-            Proxy: '@/shared/globalOverrides/Proxy',
-            Symbol: '@/shared/globalOverrides/Symbol',
-            URL: '@/shared/globalOverrides/URL',
-            WeakMap: '@/shared/globalOverrides/WeakMap',
-        })
+        ...[
+            // Custom parser to find and replace all arrayExpressions "[]" to "new Array()"
+            // This will allow the further plugin to replace it with custom global override
+            replaceArrayExpressionToConstructor(),
+            // Overriding globals with custom objects. Copying and saving original prototype functions
+            // This way we can avoid prototype overrides. Will always call original function
+            // Some functions/properties might be undefined, need to check manually
+            inject({
+                // files called "unsafe.***" will be ignored
+                include: /(?!.*\/(unsafe\.|globalOverrides))(src\/.*)|(node_modules)/,
+                Array: '@/shared/globalOverrides/Array',
+                console: '@/shared/globalOverrides/console',
+                window: '@/shared/globalOverrides/window',
+                document: '@/shared/globalOverrides/document',
+                Error: '@/shared/globalOverrides/Error',
+                KeyboardEvent: '@/shared/globalOverrides/KeyboardEvent',
+                Map: '@/shared/globalOverrides/Map',
+                Math: '@/shared/globalOverrides/Math',
+                MessageChannel: '@/shared/globalOverrides/MessageChannel',
+                MouseEvent: '@/shared/globalOverrides/MouseEvent',
+                MutationObserver: '@/shared/globalOverrides/MutationObserver',
+                Number: '@/shared/globalOverrides/Number',
+                Object: '@/shared/globalOverrides/Object',
+                Promise: '@/shared/globalOverrides/Promise',
+                Proxy: '@/shared/globalOverrides/Proxy',
+                Symbol: '@/shared/globalOverrides/Symbol',
+                URL: '@/shared/globalOverrides/URL',
+                WeakMap: '@/shared/globalOverrides/WeakMap',
+            }),
+        ]
     );
 
 export default defineConfig({
