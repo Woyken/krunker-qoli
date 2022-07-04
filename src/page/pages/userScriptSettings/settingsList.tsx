04043ba511e8fabf9510a628c13703169130027d
Woyken <13166802+Woyken@users.noreply.github.com>
2022-07-04 23:15:31 +0300

    using stack component instead of line breaks

--- src/page/pages/userScriptSettings/settingsList.tsx
+++ src/page/pages/userScriptSettings/settingsList.tsx
@@ -13,6 +13,7 @@ import {
 import Box from '@suid/material/Box';
 import Checkbox from '@suid/material/Checkbox';
 import FormControlLabel from '@suid/material/FormControlLabel';
+import Stack from '@suid/material/Stack';
 import useSavedSettings from './state/userScriptSettings';
 
 export default function SettingsList() {
@@ -21,70 +22,69 @@ export default function SettingsList() {
     return (
         <>
             <Box sx={{ width: '100%' }}>
-                <FormControlLabel
-                    value="Auto reload"
-                    control={
-                        <Checkbox
-                            checked={enabledAutoReload()}
-                            onChange={(event, checked) => {
-                                setEnabledAutoReload(checked);
-                            }}
-                        />
-                    }
-                    label="Auto reload"
-                />
-                <br />
-                <FormControlLabel
-                    value="Auto respawn"
-                    control={
-                        <Checkbox
-                            checked={enabledFastRespawn()}
-                            onChange={(event, checked) => {
-                                setEnabledFastRespawn(checked);
-                            }}
-                        />
-                    }
-                    label="Auto respawn"
-                />
-                <br />
-                <FormControlLabel
-                    value="Remove popup ads"
-                    control={
-                        <Checkbox
-                            checked={enabledAdPopupDismisser()}
-                            onChange={(event, checked) => {
-                                setEnabledAdPopupDismisser(checked);
-                            }}
-                        />
-                    }
-                    label="Remove popup ads"
-                />
-                <br />
-                <FormControlLabel
-                    value="Open Krunker in new Window"
-                    control={
-                        <Checkbox
-                            checked={enabledWindowManager()}
-                            onChange={(event, checked) => {
-                                setEnabledWindowManager(checked);
-                            }}
-                        />
-                    }
-                    label="Open Krunker in new Window"
-                />
-                <br />
-                <FormControlLabel
-                    value="Auto collect junk (click when prompted)"
-                    control={
-                        <Checkbox
-                            checked={enabledAutoClickJunkPickup()}
-                            onChange={(event, checked) => {
-                                setEnabledAutoClickJunkPickup(checked);
-                            }}
-                        />
-                    }
-                    label="Auto collect junk (click when prompted)"
-                />
+                <Stack>
+                    <FormControlLabel
+                        value="Auto reload"
+                        control={
+                            <Checkbox
+                                checked={enabledAutoReload()}
+                                onChange={(event, checked) => {
+                                    setEnabledAutoReload(checked);
+                                }}
+                            />
+                        }
+                        label="Auto reload"
+                    />
+
+                    <FormControlLabel
+                        value="Auto respawn"
+                        control={
+                            <Checkbox
+                                checked={enabledFastRespawn()}
+                                onChange={(event, checked) => {
+                                    setEnabledFastRespawn(checked);
+                                }}
+                            />
+                        }
+                        label="Auto respawn"
+                    />
+                    <FormControlLabel
+                        value="Remove popup ads"
+                        control={
+                            <Checkbox
+                                checked={enabledAdPopupDismisser()}
+                                onChange={(event, checked) => {
+                                    setEnabledAdPopupDismisser(checked);
+                                }}
+                            />
+                        }
+                        label="Remove popup ads"
+                    />
+                    <FormControlLabel
+                        value="Open Krunker in new Window"
+                        control={
+                            <Checkbox
+                                checked={enabledWindowManager()}
+                                onChange={(event, checked) => {
+                                    setEnabledWindowManager(checked);
+                                }}
+                            />
+                        }
+                        label="Open Krunker in new Window"
+                    />
+                    <FormControlLabel
+                        value="Auto collect junk (click when prompted)"
+                        control={
+                            <Checkbox
+                                checked={enabledAutoClickJunkPickup()}
+                                onChange={(event, checked) => {
+                                    setEnabledAutoClickJunkPickup(checked);
+                                }}
+                            />
+                        }
+                        label="Auto collect junk (click when prompted)"
+                    />
+                </Stack>
             </Box>
         </>
     );
